7aa690d1b861a6321fd7e7baee7a1c6d85910820
oneminuteman <victor.ireri@student.moringaschool.com>
2023-03-15 10:31:36 +0300

    my first project

---
+++ indexTest.js
@@ -0,0 +1,11 @@
+describe('index.js', function () {
+    // there's stuff in here, too
+});
+
+it('is set as Scuber', function () {
+    // tests are here
+  });
+  it('is set as Scuber', function () {
+    expect(companyName).to.equal('Scuber');
+  });
+  
